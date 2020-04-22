09519d9c4a9fae84ecb87876f85390bd0a9cedc6
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-04-21 20:28:39 -0700

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,18 @@
 ## code your solution here. 
+require 'pry'
+class Cat 
+    attr_accessor :cat
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def meow=(meow)
+        @meow = meow
+    end 
+    def meow 
+
+        puts "meow!"
+    end 
+end 
